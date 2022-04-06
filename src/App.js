8018c171ad56d1b3c84418ec696bdfc98b723a58
tomaszaugustyniak <tomasz.augustyniak2000@gmail.com>
2022-04-06 13:21:27 +0200

    first updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
 <div className="App">
   <Navbar/>
+  <Header/>
 </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,17 @@ import "./Navbar.scss";
 function Navbar() {
 
     return (
-   <div className="navbar">
-        <div className="navbar-container">
+   <section className="navbar sticky">
+        <nav className="navbar-container">
            <a href="/" className="navbar-logo">POW KATOWICE</a>
            <ul className="navbar-menu">
-             <a href="" className="navbar-menu-link">   <li className="navbar-menu-link">o nas</li> </a>
-             <a href="" className="navbar-menu-link">   <li className="navbar-menu-link">nasze placówki</li></a>
-             <a href="" className="navbar-menu-link">   <li className="navbar-menu-link">aktualności</li></a>
+             <a href="/" className="navbar-menu-link">   <li className="navbar-menu-link">o nas</li> </a>
+             <a href="/" className="navbar-menu-link">   <li className="navbar-menu-link">nasze placówki</li></a>
+             <a href="/" className="navbar-menu-link">   <li className="navbar-menu-link">aktualności</li></a>
            </ul>
-        </div>
-    </div>
+           <button className="navbar-menu-toggle">btn</button>
+        </nav>
+    </section>
   )
 }
 
